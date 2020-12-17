dd34909c446f49799b99ae1601165807d8324ec9
mahoo <mahohayashi.miwalab@gmail.com>
2020-12-17 23:47:29 +0900

    use 2 display

--- arduino/translatorMask/translatorMask.ino
+++ arduino/translatorMask/translatorMask.ino
@@ -3,6 +3,11 @@
 #include "SPI.h"
 #include "TFT_eSPI.h"
 
+#define BLE_MODE_TEXT 'A'
+#define BLE_MODE_DRAW_S 'B'
+#define BLE_MODE_DRAW 'C'
+#define BLE_MODE_DRAW_E 'D'
+
 static String DEVICE_NAME = "TranslatorMask";
 static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
 static BLEUUID    charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
@@ -11,7 +16,13 @@ bool deviceConnected = false;           // デバイスの接続状態
 bool bInAlarm  = false;                 // デバイス異常判定
 
 TFT_eSPI tft = TFT_eSPI();
+#define FIRST_SCREEN 5
+#define SECOND_SCREEN 21
+
 String displayStr = "";
+bool isDrawing = false;
+bool isDraw = false;
+int pos_x, pos_y, pre_x, pre_y;
 
 //SET UP BLE /////////////////////////////////
 //callback for server 
@@ -31,12 +42,47 @@ class functionCharaCallBacks : public BLECharacteristicCallbacks{
   }
   void onWrite(BLECharacteristic* pCharacteristic){
     std::string str = pCharacteristic->getValue();
-    displayStr = str.c_str();
-    for(int i = 0; i < str.length(); i++){
-      Serial.print(str[i]);
+    char head = str[0];
+    str =  str.erase(0,1);
+
+    if(head == BLE_MODE_TEXT){
+       displayStr = str.c_str();
+//       for(int i = 0; i < str.length(); i++){
+//         Serial.print(str[i]);
+//       }
+//       Serial.println("");
     }
-     Serial.println("");
+    else if(head == BLE_MODE_DRAW_S){
+      
+      isDrawing = true;
+      int token = str.find(",");
+      String str_y = (str.substr(token + 1)).c_str();;
+      String str_x = (str.erase(token)).c_str();
+      pre_x = str_x.toInt();
+      pre_y = str_y.toInt();
+      pos_y = pre_x;
+      pos_x = pre_y;
+     
+    }else if(head == BLE_MODE_DRAW){
+       isDraw = true;
+       int token = str.find(",");
+       String str_y = (str.substr(token + 1)).c_str();;
+       String str_x = (str.erase(token)).c_str();
+       pos_x = str_x.toInt();
+       pos_y = str_y.toInt();
+       Serial.println("update");
+       if(!isDrawing){
+         pre_x = pos_x;
+         pre_y = pos_y;
+         isDrawing = true;
+       }
+       
+    }else if(head == BLE_MODE_DRAW_E){
+      
+       isDrawing = false;
+    }
   }
+  
 };
 //setup
 void setupBLE(){
@@ -61,23 +107,62 @@ void setupBLE(){
   pAdvertising->start();
 }
 
+//TFT////////////////////////////////////////
+void setupTFT(){
+  pinMode(FIRST_SCREEN, OUTPUT);
+  digitalWrite(FIRST_SCREEN, HIGH);
+  pinMode(SECOND_SCREEN, OUTPUT);
+  digitalWrite(SECOND_SCREEN, HIGH);
+  digitalWrite(FIRST_SCREEN, LOW);
+  digitalWrite(SECOND_SCREEN, LOW);
+  
+  tft.begin();
+  tft.setRotation(1);
+  tft.setFreeFont(FF18);
+  tft.setTextSize(30);
+  digitalWrite(FIRST_SCREEN, HIGH);
+  digitalWrite(SECOND_SCREEN, HIGH);
+  
+}
+
+
 
 //////////////////////////////////////////////
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
-  tft.begin();
-  tft.setRotation(1);
-  tft.setFreeFont(FF18);
-  
+  Serial.begin(9600);
+ 
+  setupTFT();
   setupBLE();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tft.fillScreen(TFT_BLACK);
-  tft.drawString("TEST WRITING",10,10);
-  tft.drawString(displayStr,10,50);
-  delay(1000);
+
+
+  if(isDraw && isDrawing){
+    digitalWrite(FIRST_SCREEN, LOW);
+    tft.drawLine(pre_x, pre_y, pos_x, pos_y, TFT_WHITE);
+    pre_x = pos_x;
+    pre_y = pos_y;
+    isDraw = false;
+  }else{
+    digitalWrite(FIRST_SCREEN, LOW);
+    //tft.fillScreen(TFT_BLACK);
+    //tft.drawString("TEST WRITING",10,10);
+    tft.drawString(displayStr,10,50);
+    digitalWrite(FIRST_SCREEN, HIGH);
+  
+    digitalWrite(FIRST_SCREEN, LOW);
+    //tft.fillScreen(TFT_BLACK);
+    //tft.drawString("TEST WRITING",10,10);
+    tft.drawString(displayStr,10,50);
+    digitalWrite(FIRST_SCREEN, HIGH);
+  
+    delay(1000);
+  }
+
+  
+  
 }
